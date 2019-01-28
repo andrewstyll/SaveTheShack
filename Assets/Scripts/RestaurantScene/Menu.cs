44153ee257bd6597468043235dd6c86084ffc4a0
andrewstyll <andrewstyll@gmail.com>
2019-01-28 04:21:52 -0500

    customer with menu and food stubs

---
+++ Assets/Scripts/RestaurantScene/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu {
+
+    // use dictionary to map objects or something??? 
+    // menu is enum, map food to properties?? should not hold actual objects
+    private FoodStub main;
+    private List<FoodStub> toppings;
+    private List<FoodStub> drinks;
+
+    public FoodStub GetMain() {
+        return main;
+    }
+
+    public List<FoodStub> GetToppings() {
+        return toppings;
+    }
+
+    public List<FoodStub> GetDrinks() {
+        return drinks;
+    }
+}
