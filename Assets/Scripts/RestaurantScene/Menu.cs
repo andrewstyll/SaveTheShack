af2f6de7be6c43e5c8d6da9c2ddd04f301b247c4
andrewstyll <andrewstyll@gmail.com>
2019-02-05 16:28:39 -0500

    added new menu class replacing struct and modified restaurant info const file

---
+++ Assets/Scripts/RestaurantScene/Menu.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu {
+    private Food main;
+    private Food[] toppings;
+    private Food[] drinks;
+}
